65877dea4143e7f451f55cdc0e0d1ab5e087389c
zapion <zapionator@gmail.com>
2015-08-06 09:56:45 +0800

    draft

--- minions.py
+++ minions.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 
 from subprocess import Popen, PIPE
+from enum import Enum
+
+
+class status(Enum):
+    ok = 0
+    warning = 1
+    critical = 2
+    unknown = 3
 
 
 def shell_cmd(cmd):
@@ -22,15 +30,14 @@ class Minion(object):
     # 2 func types:
     #       if func is string, run it as a system command
     name = None
-    interval = None
 
-    def __init__(self, name, func=None):
+    def __init__(self, name, **kwargs):
         self.name = name
 
-    def _work(self):
+    def _work(self, **kwargs):
         '''
         Abtract private interface
-        Return True if work is done successfully
+        Return dict with status code if work is done successfully
         '''
         raise NotImplementedError(
             "%s's worker function is not yet implemented." % (self.__class__)
@@ -43,11 +50,16 @@ class Minion(object):
         Return: dict{}
         '''
         banana = {}
+        try:
+            self._work()
+        except:
+            
         self.banana = banana
         return banana
 
     def report(self):
         '''
+        TODO: if we use shinken or other framework they provide full stack of feature
         interface for emitting state to file, DB, or other storage
         Parameters: None
         Return: True if emitting successfully
@@ -56,9 +68,9 @@ class Minion(object):
         return True
 
 
-class PeriodicalMinion(Minion):
-    # Do periodical job
-    def __init__(self, name, func=None, interval=300):
-        if interval <= 0:
-            raise ValueError("inverval should be greater than 0")
-        self.interval = interval
+class DemoMinion(Minion):
+    '''
+    Demo minion for running b2g-ps
+    '''
+    def _work(self, serial):
+        ret = shell_cmd("adb devices")
